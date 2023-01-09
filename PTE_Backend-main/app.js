23429d28cefa14ff4ef2f446a19698fb10cc6d5f
ZineddineBK-DEV <zineddineboubaker20@gmail.com>
2023-01-09 11:17:01 +0100

    connection string update

--- PTE_Backend-main/app.js
+++ PTE_Backend-main/app.js
@@ -21,10 +21,20 @@ const virtualizationEnvRoute = require("./src/routes/material_resources/virtuali
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(userData);
 
-// configuring cors
-//app.use(cors);
+app.use((req,res,next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control");
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method){
+    res.status(200).send();
+  }
+  else {
+    next();
+  }
+})
+
 
 //=========== connecting to database ==============
 mongoose.set("strictQuery", true);
