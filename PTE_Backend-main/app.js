6d66c47ce9859fec9081c8ce5e1641faf79d6d95
ZineddineBK-DEV <zineddineboubaker20@gmail.com>
2023-02-08 15:59:35 +0100

    Clean app.js

--- PTE_Backend-main/app.js
+++ PTE_Backend-main/app.js
@@ -66,47 +66,5 @@ app.use("/api/material/vehicle", vehicleRoute);
 app.use("/api/material/virtualization", virtualizationEnvRoute);
 app.use("/api/career", careerRoute);
 
-
-// const server=createServer(app);
-
-// const io = new Server(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "*",
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   //connected to correct id
-//   socket.on("setup", (userData) => {
-//     socket.join(userData._id);
-
-//     socket.emit("connected");
-//   });
-
-//   socket.on("join-chat", (room) => {
-//     socket.join(room);
-//   });
-
-//   socket.on("typing", (room) => socket.in(room).emit("typing"));
-//   socket.on("stop-typing", (room) => socket.in(room).emit("stop-typing"));
-
-//   socket.on("new-message", (newMessageReceived) => {
-//     let chat = newMessageReceived.chat;
-
-//     if (!chat.users) return console.log(`chat.users not defined`);
-
-//     chat.users.forEach((user) => {
-//       if (user._id === newMessageReceived.sender._id) return;
-
-//       socket.in(user._id).emit("message-received", newMessageReceived);
-//     });
-//   });
-
-//   socket.off("setup", () => {
-//     socket.leave(userData._id);
-//   });
-// });
-
 // ======== exporting app ========
 module.exports = app;
