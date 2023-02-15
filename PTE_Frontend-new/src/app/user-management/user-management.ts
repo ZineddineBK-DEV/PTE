8abbed4b771260c1261fcaec4691599ad3140f2e
ZineddineBK-DEV <zineddineboubaker20@gmail.com>
2023-02-15 16:54:01 +0100

    template upload

---
+++ PTE_Frontend-new/src/app/user-management/user-management.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-management-list',
+  templateUrl: './user-management.html',
+  styleUrls: ['./user-management.css']
+})
+export class TableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
